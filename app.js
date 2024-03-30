ce9922fd8a499b2eba535481c4c0cffd5ed04385
Vrishti Sharma <111180589+code-it-all@users.noreply.github.com>
2024-03-30 10:56:29 +0000

    part2

--- app.js
+++ app.js
@@ -3,5 +3,24 @@ const app = express();
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
-require('dotenv').config();
+const dotenv = require('dotenv');
 var cors = require('cors');
+
+dotenv.config();
+// console.log(process.env.DATABASE)
+// //database connection 
+mongoose.connect(process.env.DATABASE).then(()=> console.log("Db connected"))
+.catch((err)=>console.log(err));
+
+
+app.use(morgan('dev'))
+app.use(bodyParser.json({limit: '5mb'}))
+app.use(bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true
+}))
+app.use(cookieParser());
+app.use(cors());
+
+let port = process.env.PORT || 8000;
+app.listen(port, ()=> console.log('Server running at port 9000.'))
