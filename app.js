a29f7077ce1e1f2a9b867bc387d8eaa2dbc680d4
code-it-all <111180589+code-it-all@users.noreply.github.com>
2024-04-14 23:29:35 +0530

    Authentication completed

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const dotenv = require('dotenv');
-var cors = require('cors');
-const errorHandler = require('./middleware/error')
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+var cors = require("cors");
+const errorHandler = require("./middleware/error");
+const dotenv = require("dotenv");
 dotenv.config();
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 
-const authRoutes = require('./routes/authRoutes')
-
+const authRoutes = require("./routes/authRoutes");
 
 // console.log(process.env.DATABASE)
-// //database connection 
-mongoose.connect(process.env.DATABASE).then(()=> console.log("Db connected"))
-.catch((err)=>console.log(err));
+// //database connectionnp
+mongoose
+  .connect("mongodb://localhost:27017/jobZee")
+  .then(() => {
+    console.log("DB CONNECTED");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-
-app.use(morgan('dev'))
-app.use(bodyParser.json({limit: '5mb'}))
-app.use(bodyParser.urlencoded({
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(
+  bodyParser.urlencoded({
     limit: "5mb",
-    extended: true
-}))
+    extended: true,
+  })
+);
 app.use(cookieParser());
 app.use(cors());
 
-
-app.use('/', authRoutes)
-
+app.use("/", authRoutes);
 
 //middleware for error handling
-app.use(errorHandler)
+app.use(errorHandler);
 
-
-let port = process.env.PORT || 8000;
-app.listen(port, ()=> console.log('Server running at port 8000.'))
+// let port = process.env.PORT || 3001;
+const port = 9000;
+app.listen(port, () => console.log("Server running at port "));
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const { signin } = require('../controllers/authController');
+const express = require("express");
+const { signup, signin, logout } = require("../controllers/authController");
 const router = express.Router();
 
 //auth routes
-router.get('/', signin)
+// router.get("/", signin);
 
+// signup route
+router.post("/signup", signup);
+
+// signin route
+router.post("/signin", signin);
+// /api/logout
+router.get("/logout", logout);
 
 module.exports = router;
