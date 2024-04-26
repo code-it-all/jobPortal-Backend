8de0e6854769235eb9e0f9bf66565e80b991d523
code-it-all <111180589+code-it-all@users.noreply.github.com>
2024-04-26 17:39:18 +0530

    updated

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const dotenv = require('dotenv');
-var cors = require('cors');
-const errorHandler = require('./middleware/error')
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+var cors = require("cors");
+const errorHandler = require("./middleware/error");
 dotenv.config();
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 
-const authRoutes = require('./routes/authRoutes')
-
+const authRoutes = require("./routes/authRoutes");
 
 // console.log(process.env.DATABASE)
-// //database connection 
-mongoose.connect(process.env.DATABASE).then(()=> console.log("Db connected"))
-.catch((err)=>console.log(err));
+// //database connection
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log("Db connected"))
+  .catch((err) => console.log(err));
 
-
-app.use(morgan('dev'))
-app.use(bodyParser.json({limit: '5mb'}))
-app.use(bodyParser.urlencoded({
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(
+  bodyParser.urlencoded({
     limit: "5mb",
-    extended: true
-}))
+    extended: true,
+  })
+);
 app.use(cookieParser());
 app.use(cors());
 
-
-app.use('/', authRoutes)
-
+app.use("/", authRoutes);
 
 //middleware for error handling
-app.use(errorHandler)
+app.use(errorHandler);
 
-
-let port = process.env.PORT || 8000;
-app.listen(port, ()=> console.log('Server running at port 8000.'))
+let port = process.env.PORT || 3001;
+app.listen(port, () => console.log("Server running at port 3001."));
