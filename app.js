5906c46b8170451d2350cc6169a014afc305e19b
Vrishti Sharma <riyavrishti@gmail.com>
2025-02-01 12:21:54 +0530

    next commit

--- app.js
+++ app.js
@@ -3,29 +3,21 @@ const app = express();
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-<<<<<<< HEAD
-const dotenv = require("dotenv");
-var cors = require("cors");
-const errorHandler = require("./middleware/error");
-=======
 
 var cors = require("cors");
 const errorHandler = require("./middleware/error");
 const dotenv = require("dotenv");
->>>>>>> a29f7077ce1e1f2a9b867bc387d8eaa2dbc680d4
 dotenv.config();
 const cookieParser = require("cookie-parser");
 
 const authRoutes = require("./routes/authRoutes");
 
 // console.log(process.env.DATABASE)
-<<<<<<< HEAD
 // //database connection
 mongoose
   .connect(process.env.DATABASE)
   .then(() => console.log("Db connected"))
   .catch((err) => console.log(err));
-=======
 // //database connectionnp
 mongoose
   .connect("mongodb://localhost:27017/jobZee")
@@ -35,7 +27,6 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
->>>>>>> a29f7077ce1e1f2a9b867bc387d8eaa2dbc680d4
 
 app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "5mb" }));
@@ -53,11 +44,6 @@ app.use("/", authRoutes);
 //middleware for error handling
 app.use(errorHandler);
 
-<<<<<<< HEAD
-let port = process.env.PORT || 3001;
-app.listen(port, () => console.log("Server running at port 3001."));
-=======
 // let port = process.env.PORT || 3001;
 const port = 9000;
 app.listen(port, () => console.log("Server running at port "));
->>>>>>> a29f7077ce1e1f2a9b867bc387d8eaa2dbc680d4
